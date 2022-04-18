3e74152039dadc5f161e7603f79ca50735032150
Megan <meganphaunef14@yahoo.com>
2022-04-18 18:59:35 -0400

    Created javascript arrays, functions, and objects

--- arrays.js
+++ arrays.js
@@ -3,14 +3,35 @@
 // 1. printIndices
 function printIndices(items) {
   // Replace this with your code
+  for (let i = 0; i < items.length; i++) {
+    console.log(`${item[i]} ${i}`);
+  }
 }
+
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
   // Replace this with your code
+  let result = []
+  for (let i = 0; i < items.length; i++) {
+    if (i % 2 === 0) { 
+      result.push(items[i]);
+    }
+  }
+  return result;
 }
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
   // Replace this with your code
-}
+  let smallest = []
+  items.sort();
+  for (let i = 0; i < n ; i++) {
+    smallest.push(items[i]);
+  } 
+  return smallest.reverse()
+  }
+
+// smallestNItems(items, n)
+// 1,2,3 
+// [2,1]
--- objects.js
+++ objects.js
@@ -3,9 +3,45 @@
 // 1. countWords
 function countWords(phrase) {
   // Replace this with your code
+  
+  // create a dictionary to store words and counts per word
+  let word_counts = {}
+
+  // convert string to array
+  let phraseArray = phrase.split(" ");
+
+  // iterate over array 
+  for (const word of phraseArray) {
+    if (word in word_counts === false) {
+      word_counts[word] = 1
+    }
+    else {
+      word_counts[word] += 1
+    }
+  // check word if in word_counts
+  // if it is, then word_counts[word] += 1
+  // else word_counts[word] = 1 
+  }
+  return word_counts
 }
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
   // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  }    
+  // console.log(price)
+  for (const key in melonPrices) {
+    if (price in melonPrices === false) {
+      return;
+    }
+  }
+  return melonPrices[price].sort()
 }
+
+// console.log(getMelonsAtPrice(1.00));
+// console.log(getMelonsAtPrice(2.50));
